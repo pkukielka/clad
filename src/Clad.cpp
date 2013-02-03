4285fdd2a2a95c5bd0ca7058981c3df8276c902f
Piotr Kukielka <piotr.kukielka@gmail.com>
2013-02-03 22:41:29 +0100

    Add support for cross-origin XMLHttpRequests

--- src/Clad.cpp
+++ src/Clad.cpp
@@ -1,8 +1,9 @@
 #include <clang-c/Index.h>
-#include <thrift/protocol/TBinaryProtocol.h>
-#include <thrift/server/TSimpleServer.h>
+#include <thrift/protocol/TJSONProtocol.h>
+#include <thrift/server/TThreadedServer.h>
 #include <thrift/transport/TServerSocket.h>
-#include <thrift/transport/TBufferTransports.h>
+
+#include "server/THttpServer.h"
 #include "generated/Clang.h"
 #include "ClangHandler.h"
 
@@ -18,10 +19,10 @@ int main(int, char**) {
   shared_ptr<ClangHandler> handler(new ClangHandler());
   shared_ptr<TProcessor> processor(new ::clad::ClangProcessor(handler));
   shared_ptr<TServerTransport> serverTransport(new TServerSocket(port));
-  shared_ptr<TTransportFactory> transportFactory(new TBufferedTransportFactory());
-  shared_ptr<TProtocolFactory> protocolFactory(new TBinaryProtocolFactory());
+  shared_ptr<TTransportFactory> transportFactory(new THttpServerTransportFactory());
+  shared_ptr<TProtocolFactory> protocolFactory(new TJSONProtocolFactory());
 
-  TSimpleServer server(processor, serverTransport, transportFactory, protocolFactory);
+  TThreadedServer server(processor, serverTransport, transportFactory, protocolFactory);
   server.serve();
   return 0;
 }
